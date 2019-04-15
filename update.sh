9b508db21e41c4c9d1d7e201e783ec1a0e41b2cc
opa334 <opa334@users.noreply.github.com>
2019-04-15 18:27:19 +0200

    Add beta repo, add Safari Plus 1.7~b1

--- update.sh
+++ update.sh
@@ -1,3 +1,9 @@
 dpkg-scanpackages -m ./debs > Packages
 bzip2 -c Packages > Packages.bz2
 gzip -c Packages > Packages.gz
+
+dpkg-scanpackages -m ./beta/debs > ./beta/Packages
+bzip2 -c ./beta/Packages > ./beta/Packages.bz2
+gzip -c ./beta/Packages > ./beta/Packages.gz
+
+cd ..
